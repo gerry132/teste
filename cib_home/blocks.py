32fa768b0eec0e6d54cc719f2cc3a5847f3fd592
Sina <sibanan@gmail.com>
2023-11-23 16:39:02 +0000

    Added call out cards and its template

---
+++ cib_home/blocks.py
@@ -0,0 +1,23 @@
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class CallOutBlock(blocks.StructBlock):
+
+    calloutcards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("icon", ImageChooserBlock(required=False)),
+                ("title", blocks.CharBlock(required=True)),
+                ("text", blocks.TextBlock(required=True)),
+                ("button_page", blocks.PageChooserBlock(required=False,
+                                                        help_text="This button has higher priority than button_url")),
+                ("button_url", blocks.URLBlock(required=False,))
+            ]
+        )
+    )
+
+    class Meta:  # noqa
+        template = "patterns/blocks/cards/callout.html"
+        icon = "placeholder"
+        label = "Call out"
