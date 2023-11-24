e6b0abecaea04665b7af9bb6ede6719ebe4ada16
Sina <sibanan@gmail.com>
2023-11-24 12:08:25 +0000

    Change textblock to richtextblock and Added button text

--- cib_home/blocks.py
+++ cib_home/blocks.py
@@ -10,10 +10,11 @@ class CallOutBlock(blocks.StructBlock):
                 ("icon", ImageChooserBlock(required=False,
                                            help_text="The Icon size should be 55x55 pixels")),
                 ("title", blocks.CharBlock(required=True)),
-                ("text", blocks.TextBlock(required=True)),
+                ("text", blocks.RichTextBlock(required=True)),
                 ("button_page", blocks.PageChooserBlock(required=False,
                                                         help_text="This button has higher priority than button_url")),
-                ("button_url", blocks.URLBlock(required=False,))
+                ("button_url", blocks.URLBlock(required=False,)),
+                ("button_text", blocks.CharBlock(required=True)),
             ]
         )
     )
