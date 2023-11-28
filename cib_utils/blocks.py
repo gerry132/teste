6fc2cdb5ca35ab865045c185207341161357ecf0
Gerry Mcbride <gerry.mcbride@trurisk.io>
2023-11-28 20:40:12 +0000

    Added base hero elements

--- cib_utils/blocks.py
+++ cib_utils/blocks.py
@@ -1,4 +1,17 @@
+from django.forms.utils import ErrorList
+from django.utils.translation import gettext_lazy as _
+
 from wagtail import blocks
+from wagtail.blocks.struct_block import StructBlockValidationError
+
+
+
+ALT_HELP_TEXT = """A short one-sentence literal description
+                    of the %s is
+                    required to make the page accessible to the
+                    visually impaired. Details: https://axesslab.com/alt-texts/"""
+
+ALT_BUTTON = ALT_HELP_TEXT % 'button'
 
 
 class BaseBlockLinkContext(blocks.StructBlock):
@@ -33,3 +46,101 @@ class BaseBlockLinkContext(blocks.StructBlock):
             context["link_title"] = None
 
         return context
+
+
+class LinkBlockWithURL(BaseBlockLinkContext):
+    """
+    Link block with url
+    """
+    page = blocks.PageChooserBlock(required=False)
+    title = blocks.CharBlock(
+        help_text=_("Leave blank to use the page's own title"), required=False
+    )
+    url = blocks.URLBlock(
+        label="URL",
+        required=False,
+        help_text="An external link.",
+    )
+
+
+class LinkButtonBlock(blocks.StructBlock):
+    """
+    Link button block class
+    """
+    button_text = blocks.CharBlock(max_length=50)
+    page = blocks.PageChooserBlock(required=False)
+    url = blocks.URLBlock(
+        label="URL",
+        required=False,
+        help_text="An external link.",
+    )
+    link_button_alt = blocks.CharBlock(
+        required=False,
+        help_text=_(ALT_BUTTON),
+    )
+
+    class Meta:
+        icon = "image"
+        label = "Link Button"
+        template = "patterns/molecules/link_button/link_button.html"
+
+    def clean(self, value):
+        errors = {}
+        values = [value["page"], value["url"]]
+        # Set only a Page OR a URL
+        if sum(value is not None and value != "" for value in values) != 1:
+            message = "Please choose only a page or set a URL."
+
+            errors["page"] = ErrorList([message])
+            errors["url"] = ErrorList([message])
+
+        # A URL must have title
+        if value["url"] and not value["heading"]:
+            errors["heading"] = ErrorList(["Please set a title for the URL"])
+
+        if errors:
+            raise StructBlockValidationError(errors)
+
+        return super().clean(value)
+
+    def get_context(self, value, parent_context=None):
+        """
+        Get context helper.
+        """
+        context = super().get_context(value, parent_context=parent_context)
+        try:
+            # A block may not have a page or url field
+            if value["url"]:
+                context["link_url"] = value["url"]
+            elif value["page"]:
+                context["link_url"] = value["page"].url
+
+        except KeyError:
+            context["link_url"] = None
+        return context
+
+
+class RichTextBlock(blocks.RichTextBlock):
+    """
+    Wrapper class for richtext.
+    """
+    class Meta:
+        label = "Rich-Text"
+        template = "patterns/molecules/streamfield/blocks/paragraph_block.html"
+
+
+class BaseFeatureBlock(blocks.StructBlock):
+    """
+    Base feature block.
+    """
+    heading = blocks.CharBlock(
+        required=False,
+        help_text="Text to display as feature heading",
+        max_length=250
+    )
+    rich_text = RichTextBlock()
+    links_list = blocks.ListBlock(
+        LinkBlockWithURL(),
+        max=5
+    )
+    button = LinkButtonBlock(max=1)
--- cib_utils/models.py
+++ cib_utils/models.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 from django.utils.decorators import method_decorator
+from django.db import models
 
 from wagtail.admin.panels import (
     FieldPanel,
     MultiFieldPanel,
+    FieldRowPanel,
+    HelpPanel, TabbedInterface, ObjectList,
 )
 
-
+from wagtail.fields import StreamField
 from wagtail.models import Page
+from wagtail.images import get_image_model_string
+from cib_utils.blocks import BaseFeatureBlock
+
 from cib_utils.cache import get_default_cache_control_decorator
+
+
+
+IMAGE_MODEL = get_image_model_string()
+
+
+ALT_HELP_TEXT = """A short one-sentence literal description
+                    of the %s is
+                    required to make the page accessible to the
+                    visually impaired. Details: https://axesslab.com/alt-texts/"""
 
 
 # Apply default cache headers on this page model's serve method.
@@ -42,3 +58,45 @@ class BasePage(Page):
                            ),
                         ]
                        )
+
+
+class HeroPage(BasePage):
+    hero_image = models.ForeignKey(
+        IMAGE_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    alt_text = models.CharField(blank=True,
+                                null=True,
+                                max_length=255,
+                                help_text=ALT_HELP_TEXT)
+    hero_double_feature = StreamField(
+        [
+            ("feature_block", BaseFeatureBlock()),
+        ],
+        blank=True,
+        min_num=2,
+        max_num=2
+    )
+
+    class Meta:
+        abstract = True
+
+    hero_panel = [
+        HelpPanel("For the hero to display you must set the image field below."),
+        FieldRowPanel([
+            FieldPanel("hero_image", classname="col6"),
+        ], "Hero Media"),
+        FieldPanel("alt_text")]
+
+    content_panels = Page.content_panels
+
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading='Content'),
+        ObjectList(hero_panel, heading='Hero'),
+        ObjectList(BasePage.promote_panels, heading='Promote'),
+        ObjectList(BasePage.settings_panels, heading='Settings', classname="settings"),
+    ])
