efc2f23635796e7b333c31fd4cff39f53181b6e9
Gerry Mcbride <gerry.mcbride@trurisk.io>
2023-12-04 15:56:55 +0000

    Update ssl certs

--- infrastructure/terraform/aws/prod/main.tf
+++ infrastructure/terraform/aws/prod/main.tf
@@ -737,9 +737,9 @@ resource "aws_lb_target_group" "default_admin" {
 
 
 resource "aws_acm_certificate" "cib_content" {
-  domain_name                 = "cib.testing.gov.ie"
+  domain_name                 = "prod.cib.testing.gov.ie"
   subject_alternative_names = [
-     "www.cib.testing.gov.ie"
+     "www.prod.cib.testing.gov.ie"
   ]
   validation_method           = "DNS"
 
@@ -749,7 +749,7 @@ resource "aws_acm_certificate" "cib_content" {
 }
 
 resource "aws_acm_certificate" "cib_content_admin" {
-  domain_name                 = "content.cib.testing.gov.ie"
+  domain_name                 = "prod.content.cib.testing.gov.ie"
   validation_method           = "DNS"
 
   lifecycle {
