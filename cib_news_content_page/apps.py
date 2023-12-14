4dddfbb1db6f28e8d89376752ed38f01216b4a80
Sina <sibanan@gmail.com>
2023-12-14 12:13:47 +0000

    Added banner image style

--- cib_news_content_page/apps.py
+++ cib_news_content_page/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CibNewsContentPageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cib_news_content_page'
+
+    def ready(self):
+        import cib_news_content_page.signals  # noqa
