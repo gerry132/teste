13a5857617e5b19701b8c714da3777851ee48630
Sidero Ubuntu User <deekshanaveluchamy@sidero.ie>
2023-12-15 09:53:14 +0000

    update-locale-document-tags

--- cib_documents/wagtail_hooks.py
+++ cib_documents/wagtail_hooks.py
@@ -1,6 +1,7 @@
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
 from .models import YearTag, DocumentTypeTag
+from django.utils.translation import get_language
 
 
 class YearTagAdmin(ModelAdmin):
@@ -9,8 +10,8 @@ class YearTagAdmin(ModelAdmin):
     menu_icon = 'tag'
     menu_order = 900
     add_to_settings_menu = True
-    list_display = ['name',]
-    search_fields = ('name',)
+    list_display = ['name', 'description']
+    search_fields = ['name', 'description']
 
 
 modeladmin_register(YearTagAdmin)
@@ -22,8 +23,8 @@ class DocumentTypeTagAdmin(ModelAdmin):
     menu_icon = 'tag'
     menu_order = 901
     add_to_settings_menu = True
-    list_display = ['name',]
-    search_fields = ('name',)
+    list_display = ['name', 'description', 'locale']
+    search_fields = ['name', 'description']
 
 
 modeladmin_register(DocumentTypeTagAdmin)
