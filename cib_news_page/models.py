41e8d468dcf97c76a0f79dd5e130672ac8711719
Sina <sibanan@gmail.com>
2023-12-15 13:26:09 +0000

    Added template, leads field

--- cib_news_page/models.py
+++ cib_news_page/models.py
@@ -1,7 +1,9 @@
-from cib_utils.models import BasePage
+
 from django.utils.translation import gettext_lazy as _
 
+from cib_utils.models import BasePage
 from cib_images.models import CustomImage
+from cib_news_content_page.models import NewsContentPage
 
 
 class News(BasePage):
@@ -9,10 +11,28 @@ class News(BasePage):
     template = "patterns/pages/news.html"
     # parent_page_types = ["cib_home.HomePage"]
 
-    def childs(self, req="None"):
+    def childs(self, req=None):
         """Get all child based on the filter"""
-        # @todo get all chils pages attributes
-        pass
+        main_childs = NewsContentPage.objects.\
+            descendant_of(self).live().order_by("-last_published_custom")
+
+        # if req and main_childs:
+        #     if req.get('new_tags', None):
+        #         return main_childs.filter(
+        #             news_tags__slug=req.get('news_tags', None))
+        #     date = req.get('news_date', None)
+        #     try:
+        #         if date and len(date.split('-')) > 0:
+        #             return main_childs.filter(
+        #                 last_published_custom__month=date.split('-')[1],
+        #                 last_published_custom__year=date.split('-')[0])
+
+        #         year = req.get('news_year', None)
+        #         if year:
+        #             return main_childs.filter(last_published_custom__year=year)
+        #     except: # noqa
+        #         return main_childs
+        return main_childs
 
     class Meta:
         verbose_name = _("news page")
@@ -21,6 +41,7 @@ class News(BasePage):
     def get_context(self, request, *args, **kwargs):
 
         context = super().get_context(request, *args, **kwargs)
+        context["child_with_tags"] = self.childs(request)
         context['sample_image'] = CustomImage.objects.filter(title="president").first()
 
         return context
