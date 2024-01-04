655e815db080e6f2c3373b73aa02ac880382e0b3
Sidero Ubuntu User <deekshanaveluchamy@sidero.ie>
2024-01-04 11:34:58 +0000

    updated

--- cib_project_styleguide/templatetags/left_nav.py
+++ cib_project_styleguide/templatetags/left_nav.py
@@ -3,6 +3,10 @@ from django import template
 register = template.Library()
 
 
+def get_children_pages(current_page, depth):
+    return current_page.get_children().filter(depth=depth)
+
+
 @register.inclusion_tag('patterns/organisms/left_nav.html', takes_context=True)
 def left_nav(context, current_page):
     depth = current_page.depth
@@ -12,14 +16,14 @@ def left_nav(context, current_page):
     grandchild_pages = None
 
     if depth == 3:
-        children_pages = current_page.get_children().filter(depth=depth + 1)
+        children_pages = get_children_pages(current_page, depth + 1)
     elif depth == 4:
         parent_page = current_page.get_parent()
-        children_pages = parent_page.get_children().filter(depth=depth)
+        children_pages = get_children_pages(parent_page, depth)
         sub_child_pages = current_page.get_children().filter(depth=depth + 1)
     elif depth == 5:
         parent_page = current_page.get_ancestors(inclusive=True).filter(depth=depth - 2)
-        children_pages = parent_page.first().get_children().filter(depth=depth - 1)
+        children_pages = get_children_pages(parent_page.first(), depth - 1)
         sub_child_pages = current_page.get_siblings().filter(depth=depth)
         grandchild_pages = current_page.get_children().filter(depth=depth + 1)
 
